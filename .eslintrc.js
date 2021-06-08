506a4e827fdd830e16ae17ddbb071d004bc4050f
sungkwang <sungkwang@wantedlab.com>
2021-06-08 22:13:45 +0900

    add husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        node: true,
+        browser: true,
+        es2021: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
         },
-        "ecmaVersion": 12,
-        "sourceType": "module"
+        ecmaVersion: 12,
+        sourceType: 'module'
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    },
-    "ignorePatterns": ["node_modules"],
-    "settings": {
-        "react": {
-            "version": "detect"
+    plugins: ['react', '@typescript-eslint'],
+    rules: {},
+    ignorePatterns: ['node_modules'],
+    settings: {
+        react: {
+            version: 'detect'
         }
     }
 };
